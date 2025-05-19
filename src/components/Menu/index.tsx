58ca918f8f28226ec8ac68af4c93fb5dc5c2aeee
phgusmao <pedrohenriquecsa7@gmail.com>
2025-05-19 02:04:42 -0300

    feat: data theme change finished

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,19 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react'
 import styles from './styles.module.css'
 import { useState, useEffect } from 'react'
 
 type AvailableThemes = 'dark' | 'light'
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark')
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme = (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  })
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />
+  }
 
   function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
     event.preventDefault();
@@ -18,6 +26,7 @@ export function Menu() {
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   return (
@@ -33,7 +42,7 @@ export function Menu() {
           <SettingsIcon />
         </a>
         <a href="#" onClick={handleThemeChange} className={styles.menuLink} aria-label='Mudar o tema' title='Mudar o tema'>
-          <SunIcon />
+          {nextThemeIcon[theme]}
         </a>
       </nav>
     </>
