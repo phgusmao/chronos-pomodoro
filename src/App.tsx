bc468d19e6bc07608776fea67df0ea5ddc494c5e
phgusmao <pedrohenriquecsa7@gmail.com>
2025-05-27 12:28:16 -0300

    feat: create a global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react'
 import { Home } from './pages/Home'
 import './styles/global.css'
 import './styles/theme.css'
+import { TaskStateModel } from './models/TaskStateModel'
+
+// export type TaskStateModel = {
+//   tasks: TaskModel[]; //histórico, MainForm
+//   secondsRemaining: number; // Home, CountDown, histórico, MainForm, Button
+//   formattedSecondsRemaining: string; //Titulo, CountDown 
+//   activeTask: TaskModel | null;//CountDown, histórico, MainForm, Button
+//   currentCycle: number; // 1 a 8 Home
+//   config: {
+//     workTime: number; // MainForm
+//     shortBreakTime: number; //MainForm
+//     longBreakTime: number; //MainForm
+//   }
+// }
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  }
+}
 
 export function App() {
 
+  const [state, setState] = useState(initialState);
+
   return (
-    <Home />
+    <Home state={state} setState={setState} />
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
+import { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
 
+type HomeProps = {
+  state: TaskStateModel,
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+}
 
-export function Home() {
+export function Home(props: HomeProps) {
+  console.log(props)
 
   return (
     <MainTemplate>
