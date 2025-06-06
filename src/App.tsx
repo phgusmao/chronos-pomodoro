d96b509df64c3cfdb06435d219e938828c26e7b1
phgusmao <pedrohenriquecsa7@gmail.com>
2025-06-06 16:37:46 -0300

    feat: organizing context folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
 import { Home } from './pages/Home'
-import { TaskContextProvider } from './contexts/TaskContext'
 
 import './styles/global.css'
 import './styles/theme.css'
