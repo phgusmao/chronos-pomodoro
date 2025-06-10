af470a49dcf633eff3059515e946eb169b6d6d76
phgusmao <pedrohenriquecsa7@gmail.com>
2025-06-10 19:44:46 -0300

    feat: create a new task

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./taskContext";
 
@@ -9,6 +9,10 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
   const [state, setState] = useState(initialTaskState)
+
+  useEffect(() => {
+    console.log(state)
+  }, [state])
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
