8eea3f3e838d40348b23895c75c4849577a36064
phgusmao <pedrohenriquecsa7@gmail.com>
2025-06-10 21:20:59 -0300

    feat: get cycle type

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -6,13 +6,15 @@ import { useRef } from "react";
 import { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 
 export function MainForm() {
   const { state, setState } = useTaskContext()
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   // ciclos
-  const nextCycle = getNextCycle(state.currentCycle)
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -33,7 +35,7 @@ export function MainForm() {
       completeDate: null,
       interruptDate: null,
       duration: 1,
-      type: 'workTime',
+      type: nextCycleType,
     }
 
     const secondsRemaining = newTask.duration * 60
